f6d46e1d7164fad98542c8bfbc84a17f5c9725df
Tyler <nguyentyler97@gmail.com>
2022-06-29 15:25:39 -0700

    Draft lines of pseudocode

---
+++ javascript.js
@@ -0,0 +1,6 @@
+// Ask user for input number
+let userInput = prompt("Input a number")
+// Starting from 1, print each number up to the user's input number
+// For every number with the multiple of 3, print "Fizz" instead
+// For every number with the multiple of 5, print "Buzz" instead
+// For every number with the multiple of 3 & 5, print "FizzBuzz" instead
