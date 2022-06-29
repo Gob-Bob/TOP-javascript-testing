8059d4a7dbceff1c4cf1946b127c2ede2ed9bdab
Tyler <nguyentyler97@gmail.com>
2022-06-29 15:32:42 -0700

    Complete basic codes

--- javascript.js
+++ javascript.js
@@ -1,6 +1,18 @@
 // Ask user for input number
 let userInput = prompt("Input a number")
+
 // Starting from 1, print each number up to the user's input number
-// For every number with the multiple of 3, print "Fizz" instead
-// For every number with the multiple of 5, print "Buzz" instead
-// For every number with the multiple of 3 & 5, print "FizzBuzz" instead
+for (let i = 1; i <= userInput; i++) {
+    // For every number with the multiple of 3 & 5, print "FizzBuzz" instead
+    if (i % 3 == 0 && i % 5 == 0) {
+        console.log("FizzBuzz")
+    // For every number with the multiple of 3, print "Fizz" instead
+    } else if (i % 3 == 0) {
+        console.log("Fizz")
+    // For every number with the multiple of 5, print "Buzz" instead
+    } else if (i % 5 == 0) {
+        console.log("Buzz")
+    } else {
+        console.log(i);
+    }
+}
